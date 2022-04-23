cb47572fb7712a0069c3a92c40161abbc82a031b
Martynas Voldemaras <42231878+MartynasMV@users.noreply.github.com>
2022-04-23 19:09:38 +0200

    data populated dynamically from json

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
 import TourCard from './components/TourCard';
 import { Container } from '@mui/material';
+import { Typography } from '@mui/material';
 import { Grid } from '@mui/material';
+import SearchAppBar from './components/SearchAppBar';
+import cities from './data.json';
 
 function App() {
   return (
     <div className="App">
-      <Container maxWidth="md">
-        <Grid container spacing={5}>
-          <TourCard />
-          <TourCard />
-          <TourCard />
-          <TourCard />
-        </Grid>
+      <SearchAppBar />
+      <Container sx={{ marginY: 5 }}>
+        {cities.map((city) => (
+          <>
+            {' '}
+            <Typography
+              variant="h4"
+              component="h2"
+              marginTop={5}
+              marginBottom={3}
+            >
+              Top {city.name} Tours
+            </Typography>
+            <Grid container spacing={5}>
+              {city.tours.map((tour, index) => (
+                <TourCard tour={tour} key={index} />
+              ))}
+            </Grid>
+          </>
+        ))}
       </Container>
     </div>
   );
