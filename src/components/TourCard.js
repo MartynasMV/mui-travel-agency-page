ad7dbc50e3e0e81a53b41ed412e80867b323a3bc
Martynas Voldemaras <42231878+MartynasMV@users.noreply.github.com>
2022-04-23 16:37:35 +0200

    first c

---
+++ src/components/TourCard.js
@@ -0,0 +1,12 @@
+import { Paper } from '@mui/material';
+import { Grid } from '@mui/material';
+
+const TourCard = () => {
+  return (
+    <Grid item xs={3}>
+      <Paper elevation={3}>Hey</Paper>
+    </Grid>
+  );
+};
+
+export default TourCard;
